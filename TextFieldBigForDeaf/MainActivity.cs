e35f93038f85e0f970bc7225e4148ba2778449d4
sergey19941201 <koshelnick1994@mail.ru>
2017-03-23 21:27:44 +0200

    Done

---
+++ TextFieldBigForDeaf/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Content.PM;
+using Android.Graphics;
+
+namespace TextFieldBigForDeaf
+{
+    [Activity(MainLauncher = true, Icon = "@drawable/icon",ScreenOrientation = ScreenOrientation.Landscape, Theme = "@android:style/Theme.NoTitleBar")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            FindViewById<Button>(Resource.Id.clearBn).Click += delegate
+            {
+                FindViewById<EditText>(Resource.Id.editText).Text = "";
+            };
+        }
+
+        public override void OnBackPressed()
+        {
+            FinishAffinity();
+        }
+    }
+}
+
